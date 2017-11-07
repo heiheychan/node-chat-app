8d19caa85784f700a79f11ffc31065b859873293
heiheychan <cchei1993@gmail.com>
2017-11-07 03:11:52 -0500

    Add Geolocation

--- public/js/index.js
+++ public/js/index.js
@@ -10,9 +10,21 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (mssg) {
   console.log('New message', mssg);
-  var li = $('<li></li>');
+  var li = $('<li>');
   li.text(`${mssg.from}: ${mssg.text}`);
 
+  $('#messages').append(li);
+});
+
+socket.on('newLocationMessage', function (mssg) {
+  console.log('New location message', mssg);
+  var li = $('<li>')
+  var a = $('<a target="_blank">My Current Location</a>');
+
+  li.text(`${mssg.from}: `);
+  a.attr('href', mssg.url);
+
+  li.append(a);
   $('#messages').append(li);
 });
 
@@ -24,5 +36,21 @@ $('#message-form').on('submit', function(e) {
     text: $('input[name=message]').val()
   }, function(){
 
+  })
+})
+
+var locationButton = $('#send-location');
+locationButton.on('click', function() {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
+
+  navigator.geolocation.getCurrentPosition(function(pos) {
+    socket.emit('createLocationMessage', {
+      latitude: pos.coords.latitude,
+      longitude: pos.coords.longitude
+    });
+  }, function(err) {
+    alert('Unable to fetch location.');
   })
 })
