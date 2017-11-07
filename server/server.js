a66bc1aa309ced6b47a24bccc16e8b90cc58496f
heiheychan <cchei1993@gmail.com>
2017-11-06 23:44:42 -0500

    add extension to modules

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./util/message');
+const {generateMessage} = require('./util/message.js');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -20,9 +20,10 @@ io.on('connection', (socket) => {
 
   socket.broadcast.emit('newMessage', generateMessage('Admin','Someone has joined the conversation'));
 
-  socket.on('createMessage', (message) => {
+  socket.on('createMessage', (message, callback) => {
     console.log('New Mssg coming through', message);
     io.emit('newMessage', generateMessage(message.from,message.text));
+    callback('This is from the server.');
     // socket.broadcast.emit('newMessage', {
     //   from: message.from,
     //   text: message.text,
