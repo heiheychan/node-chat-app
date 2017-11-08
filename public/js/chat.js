bc13378e87b77a44f97e98749c98131aca98f398
heiheychan <cchei1993@gmail.com>
2017-11-08 04:33:29 -0500

    Add join page

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,7 @@
 var socket = io();
 
 function scrollToBottom () {
+  // var newMessage = $('#messages li:last-child').innerHeight();
   var scrolltop = $('#messages').prop('scrollHeight') - $('#messages').prop('clientHeight');
   $('#messages').scrollTop(scrolltop);
 }
